151bac0af18fbe2afe00a67668c087b5d702c57c
lucawrabetz <luw28@pitt.edu>
2024-02-19 14:21:47 -0500

    Added contest sample directory.

---
+++ contest-sample/F/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int T, N;
+  cin >> T;
+  for (int t = 0; t < T; ++t) {
+    int N;
+    cin >> N;
+    for (int n = 0; n < N; ++n) {
+    }
+  }
+}
